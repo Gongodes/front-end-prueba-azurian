e446e136da391961bbe66f06bf24e36ff7d2967b
Gonzalo Godoy Espinosa <gonzalo.godoy.esp@gmail.com>
2024-02-04 02:07:38 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild} from '@angular/core';
+import { TableService } from './table.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource} from '@angular/material/table';
+
+
+export interface UserData{
+id: String,
+nombre:String,
+apellido:String,
+telefono:String,
+direccion:String,
+}
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +21,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba-azurian';
+  displayedColumn: string[]=['id','nombre','apellido','telefono', 'direccion' ]
+  dataSource!: MatTableDataSource<UserData>
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+  @ViewChild(MatSort) sort!: MatSort
+  posts: any
+  constructor(private service: TableService){
+    this.service.getData().subscribe(data => {
+      console.log(data)
+
+      this.posts=data
+      this.dataSource = new MatTableDataSource(this.posts);
+      this.dataSource.paginator = this.paginator
+      this.dataSource.sort = this.sort
+    })
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import {MatTableModule} from '@angular/material/table'
+import {MatPaginatorModule} from '@angular/material/paginator'
+import {MatSortModule} from '@angular/material/sort'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input'
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -10,10 +16,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
